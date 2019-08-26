c28569f964a610f8fb7db1a1a984e70e1a5143f2
arq-gabo <jgabrielguerral@gmail.com>
2019-08-26 23:55:42 +0000

    [FIX]Corretion Betty

--- cmdbld.c
+++ cmdbld.c
@@ -9,21 +9,21 @@
 
 int cmdbld(char *buffer, char *args[])
 {
-        
+
 	char *av;
 	int i;
 
 	i = 0;
 
-	while(buffer[i])
+	while (buffer[i])
 		i++;
-                
-	buffer[i-1] = 0;
-	
+
+	buffer[i - 1] = 0;
+
 	i = 0;
-	av = NULL;      
-	av = strtok(buffer, " ");        
-	while(av)
+	av = NULL;
+	av = strtok(buffer, " ");
+	while (av)
 	{
 		args[i] = av;
 		av = strtok(NULL, " ");
