d55a650bb90e9f47facd0a99ac57b7131070dac7
German Franco F <933@holbertonschool.com>
2019-08-26 23:05:46 +0000

    [Fix] _strlen

--- pathdirs.c
+++ pathdirs.c
@@ -4,17 +4,12 @@ int pathdirs(char *path, char **dirs)
 {
 	unsigned int i;
 	char *av;
-	/*char *dirs[1024] = {0};*/
-	
-	
-	
-
 	
 	i = 0;
 	av = strtok(path, ":");
 	while (av)
 	{
-		dirs[i] = malloc(strlen(av) * sizeof(char));
+		dirs[i] = malloc(_strlen(av) * sizeof(char));
 		strcpy(dirs[i],av);
 		av = strtok(NULL, ":");
 		i++;
