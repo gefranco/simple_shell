b09dbebc3cf74a453ed6731712bb6c9f9669d21e
German Franco F <933@holbertonschool.com>
2019-08-26 23:02:57 +0000

    [Fix] enviromment from main

--- fpcmd.c
+++ fpcmd.c
@@ -5,10 +5,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "shell.h"
-char *fpcmd(char *args[])
+char *fpcmd(char *args[], char **env)
 {
-
-	char *path = _getenv("PATH");
+	char *path = _getenv("PATH", env);
         char *d[1024] = {0};
 	char *cmd = args[0];
 	char *buff;
@@ -18,19 +17,13 @@ char *fpcmd(char *args[])
 	int i = 0;
 	int j = 0;
 	int i2 = 0;
-	int k = 0;
-	int l = 0;
 	
-	
-	if(strcmp(cmd,"env") == 0)
+	if(_strcmp(cmd,"env") == 0)
 	{
-		ptrenv();
+		ptrenv(env);
 		return(NULL);
-		
 	}
 	pathdirs(path, d);
-
-
 	while(cmd[i])
 		i++;
 	
@@ -40,41 +33,14 @@ char *fpcmd(char *args[])
 		j = 0;
 		while (buff[j])
 			j++;
-		
 		f = malloc(i + j + 1 * sizeof(char));
-		k = 0;
-		l = 0;	
-		while(k < j)
-		{
-			f[k] = buff [l];
-			k++;	
-			l++;
-		}
-		f[k] = '/';
-		k++;
-		l = 0;
-		while(k < (i + j)+1)
-		{
-			f[k] = cmd[l];
-			k++;
-			l++;
-		}
-		
+		concat(buff, cmd, f);
 		if(stat(f, &st) == 0)
-		{
 			args[0] = f;
-		}
 		else
 		{
-		/*	perror("not found\n");*/
-			/*exit(0);*/
 		}
 		i2++;
-			
-		/*free(f);*/
-
 	}
-
 	return(args[0]);
 }
-
