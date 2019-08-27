0854a117ff00b7cb5258b98102b2c648fb2e138a
arq-gabo <jgabrielguerral@gmail.com>
2019-08-27 04:10:50 +0000

    [FIX]Betty Correction

--- getenv.c
+++ getenv.c
@@ -2,23 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include "shell.h"
+
+/**
+ * _getenv - Get Enviroment of the command.
+ * @name: argument for tokanizade.
+ * @environ: pointer to enviroment.
+ * Return: return enviroment.
+ */
+
 char *_getenv(const char *name, char **environ)
 {
 	unsigned int i;
 
 	/*extern char **environ;*/
-	
+
 	char *env;
 	char *av;
 	char *tmp;
+
 	env = NULL;
+
 	i = 0;
 	while (environ[i])
 	{
 		tmp = malloc(_strlen(environ[i]) * sizeof(char));
-		strcpy(tmp,environ[i]);
+		strcpy(tmp, environ[i]);
 		av = strtok(tmp, "=");
-		if (_strcmp(av,name) == 0)
+		if (_strcmp(av, name) == 0)
 		{
 			env = strtok(NULL, "=");
 			return (env);
@@ -27,7 +37,7 @@ char *_getenv(const char *name, char **environ)
 	}
 
 
-	return(env);
+	return (env);
 
 
 }
